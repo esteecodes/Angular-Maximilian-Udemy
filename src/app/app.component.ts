81fb82e98f58e3e3d09d7b565cb530a008b61066
Esther White <esteewhite@hotmail.com>
2022-06-15 14:41:03 +0300

    ngModel lessons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-maximilian-udemy';
+  title = 'Angular by Maximilian on Udemy';
+  name = 'Estee';
 }
